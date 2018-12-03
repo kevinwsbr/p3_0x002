bac9bebf9ff82030f21e2986a6833b9efb7d6d42
Kevin Washington <kevin.washingtonbr@gmail.com>
2018-12-03 16:07:05 -0300

    Initial structure

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
