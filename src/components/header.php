bcf5a33113a040142272b8de2e88cf34fb077427
Kevin Washington <kevin.washingtonbr@gmail.com>
2018-12-03 22:42:36 -0300

    Removing logout option

--- src/components/header.php
+++ src/components/header.php
@@ -2,10 +2,5 @@
 <header>
     <nav class="navbar navbar-dark bg-success mb-4">
         <a href="../index.php" class="navbar-brand">Sistema de Folha de Pagamento</a>
-        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-      <li class="nav-item">
-        <a class="nav-link active" href="../logout.php">Sair</a>
-      </li>
-    </ul>
     </nav>
 </header>
