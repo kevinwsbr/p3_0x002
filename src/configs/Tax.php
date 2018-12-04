bc2f41b9e4159b7c42bb71a86e3de4999e43663b
Kevin Washington <kevin.washingtonbr@gmail.com>
2018-12-03 22:42:57 -0300

    Initializing taxes object

---
+++ src/configs/Tax.php
@@ -0,0 +1,71 @@
+<?php
+
+class Tax {
+    protected $ID;
+    protected $value;
+    protected $date;
+    protected $employeeID;
+    protected $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function getID()
+    {
+        return $this->ID;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function getEmployeeID()
+    {
+        return $this->employeeID;
+    }
+
+    public function setData($tax)
+    {
+        $this->ID = $tax['ID'];
+        $this->time = $tax['value'];
+        $this->date = $tax['date'];
+        $this->employeeID = $tax['idemployee'];
+    }
+
+    public function getSalesFromEmployee($employeeID)
+    {
+        $sql='SELECT * FROM `taxes` WHERE `idemployee` = :idemployee;';
+
+        $db=$this->db->prepare($sql);
+        $db->bindValue(':idemployee', employeeID, PDO::PARAM_STR);
+        $db->execute();
+
+        return $db->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function register()
+    {
+        if ($_SERVER['REQUEST_METHOD']=='POST') {
+            $value = str_replace(',', '.', $_POST['value']);
+            $sql='INSERT INTO `taxes` (`value`, `date`, `idemployee`) VALUES (:value, :date, :idemployee);';
+            
+            $db=$this->db->prepare($sql);
+            $db->bindValue(':value', $value, PDO::PARAM_STR);
+            $db->bindValue(':date', $_POST['date'], PDO::PARAM_STR);
+            $db->bindValue(':idemployee', $_POST['employeeID'], PDO::PARAM_STR);
+            
+            $db->execute();
+            header('Location: index.php');
+        }
+    }
+    
+}
+?>
